0eff8530b41b60dfa03f238693948340d6923697
AdityaDudhal <a.dudhal@maxxton.com>
2025-06-11 10:06:49 +0530

    feat: implement counter component structure with buttons and output
    - Added CounterComponent with basic template and styles.
    - Created CounterButtonsComponent and CounterOutputComponent with placeholder templates.
    - Updated app.component.html to include the new CounterComponent.
    - Modified app.component.ts to import and declare the CounterComponent.
    - Added unit tests for CounterComponent, CounterButtonsComponent, and CounterOutputComponent.

---
+++ src/app/counter/counter-buttons/counter-buttons.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-buttons',
+  imports: [],
+  templateUrl: './counter-buttons.component.html',
+  styleUrl: './counter-buttons.component.css'
+})
+export class CounterButtonsComponent {
+
+}
---
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-output',
+  imports: [],
+  templateUrl: './counter-output.component.html',
+  styleUrl: './counter-output.component.css'
+})
+export class CounterOutputComponent {
+
+}
